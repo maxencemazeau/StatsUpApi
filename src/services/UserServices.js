c482e5b36e285ec471e3b30fff0cfac32d2a45b4
Samuel-Gassama <samgassa@hotmail.fr>
2024-06-13 19:12:50 -0400

    Login and SIgnup working, email in use

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const db = require('../db'); // Adjust the path as necessary
+const { error } = require('console');
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const userLoginService = async (email, password) => {
     try {
         // Query user from the database
-        const query = await db.query('SELECT * FROM User WHERE email = ?', [email]);
+        const query = await db.query('SELECT Email, Password FROM User WHERE email = ?', [email]);
         const user = query[0];
 
         // If user not found, return null
@@ -15,7 +16,6 @@ const userLoginService = async (email, password) => {
             console.log('User not found');
             return null;
         }
-
         // Compare password with hashed password
         const passwordMatch = await bcrypt.compare(password, user[0].Password);
 
@@ -39,15 +39,32 @@ const getAllUsersService = async () => {
 };
 
 const userSignUpService = async (email, username, password) => {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const query = await db.query('INSERT INTO User (email, username, password) values (?, ?, ?)', [email, username, hashedPassword]);
-    
-    if (query.affectedRows > 0) {
-        const user = { id: query.insertId, email, username };
-        const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, { expiresIn: '1h' });
-        return { user, token };
-    } else {
-        return null;
+    try {
+        // Check if email already exists
+        const existingUserQuery = await db.query('SELECT * FROM User WHERE email = ?', [email]);
+        const existingUser = existingUserQuery[0]; // Extract the first row of results
+
+        if (existingUser && existingUser.length > 0) {
+            // Email already exists
+            return { error: 'Email already in use' };
+        }
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Insert the new user into the database
+        const query = await db.query('INSERT INTO User (email, username, password) VALUES (?, ?, ?)', [email, username, hashedPassword]);
+
+        // Check if the user was successfully created
+        if (query && query.affectedRows > 0) {
+            const user = { id: query.insertId, email, username };
+            const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, { expiresIn: '1h' });
+            return { user, token };
+        } 
+
+    } catch (error) {
+        console.error('Error in userSignUpService:', error);
+        return { error: 'An unexpected error occurred' }; // Provide a generic error message
     }
 };
 
